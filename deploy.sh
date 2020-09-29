507ea95a78d47ce3199567e708a472fb62aef68d
Daniel Ratmiroff <danielratmiroff@gmail.com>
2020-09-29 19:39:43 +0200

    production styling bug

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
+# Steps to follow
+# 1. Create 'gh-pages' branch and set it has source to build in Github (Github repository's Settings)
+# 2. Create deploy.sh and replace it's ssh (git@github.com:...)
+# 3. Create vue.config.js and add following:
+#------ module.exports = {
+#------   publicPath:
+#------     process.env.NODE_ENV === "production" ? "/project-management/" : "/",
+#------ };
+# 4. Open project's folder with Git Bash and this file (run sh deploy.sh)
+
 # build
 npm run build
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  css: {
+    extract: false,
+  },
   publicPath:
     process.env.NODE_ENV === "production" ? "/project-management/" : "/",
 };
+css: {
+  extract: false;
+}
