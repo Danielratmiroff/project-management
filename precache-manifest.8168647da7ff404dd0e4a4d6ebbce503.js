7283077a80fc634763160698a200fb79d7426113
danielratmiroff <danielratmiroff@gmail.com>
2021-03-03 20:06:23 +0100

    deploy

---
+++ precache-manifest.8168647da7ff404dd0e4a4d6ebbce503.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11440f0ff1a3638088b7b418735021c",
+    "url": "/project-management/img/favicon.png"
+  },
+  {
+    "revision": "0f90d1b05af6d9efbdf334f304ecdda1",
+    "url": "/project-management/img/logo.0f90d1b0.svg"
+  },
+  {
+    "revision": "6428c43bd386bfc27a6785ec5afc8e8a",
+    "url": "/project-management/index.html"
+  },
+  {
+    "revision": "aed9790a6fc398f396c8",
+    "url": "/project-management/js/about.7f9781a1.js"
+  },
+  {
+    "revision": "06121bc5349e9d7b9d9a",
+    "url": "/project-management/js/app.d49ef45b.js"
+  },
+  {
+    "revision": "61d45f1c8ac6b82b906c",
+    "url": "/project-management/js/chunk-vendors.c54a6484.js"
+  },
+  {
+    "revision": "ef047ac3934ec1e80d3d1afe4b7dd0a6",
+    "url": "/project-management/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/project-management/robots.txt"
+  }
+]);
